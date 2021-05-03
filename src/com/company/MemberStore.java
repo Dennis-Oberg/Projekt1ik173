eaa168c868e948800ac7edc050771e091418d414
Tobias Wendel <hansson101@gmail.com>
2021-05-03 18:33:01 +0200

    numberOfBorrowedBooks fixad

---
+++ src/com/company/MemberStore.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class MemberStore {
+    ArrayList<Member> MeberList = null;
+
+    public MemberStore(){
+        this.MeberList = new ArrayList<>();
+    }
+
+    public void getMember(int id){
+
+    }
+}
