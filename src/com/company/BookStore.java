acf0fd3647683357192d38dea706f592285a2eaf
Tobias Wendel <hansson101@gmail.com>
2021-05-03 12:22:06 +0200

    Stora ändringar

--- src/com/company/BookStore.java
+++ src/com/company/BookStore.java
@@ -1,6 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class BookStore implements IBookStore {
+    ArrayList<Book> bookList = null;
+
+    public BookStore(){
+        this.bookList = new ArrayList<>();
+    }
+
     public Book[] getBooks(){
         return new Book[0];
         //select * from Books
@@ -10,5 +18,27 @@ public class BookStore implements IBookStore {
     {
         //kod mot databas som hämtar en Book
         return new Book(123, "ost");
+    }
+    public Book[] getBookByIsbn(int isbn){
+        ArrayList<Book> tempList = new ArrayList<>();
+
+        for (Book book: bookList){
+            if (book.getIsbn() == isbn){
+                tempList.add(book);
+            }
+        }
+        Book[] books = new Book[tempList.size()];
+        return tempList.toArray(books);
+    }
+    public Book[] getBookByMember (int member){
+        ArrayList<Book> tempList = new ArrayList<>();
+
+        for (Book book: bookList){
+            if (book.getBorrowedBy() == member){
+                tempList.add(book);
+            }
+        }
+        Book[] books = new Book[tempList.size()];
+        return tempList.toArray(books);
     }
 }
