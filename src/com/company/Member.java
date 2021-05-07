4f4092f4974be07a08b7eb391703332f328f1eca
Tobias Wendel <hansson101@gmail.com>
2021-05-07 21:04:46 +0200

    saknad klass är nu med

---
+++ src/com/company/Member.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Member extends User{
+
+    AuthService auth;
+    User user;
+    BookStore bStore;
+    BookManager bManager;
+
+    public Member(AuthService loggedinUser){
+        auth = loggedinUser;
+        user = loggedinUser.getLoggedInMember();
+        bStore = new BookStore();
+        bManager = new BookManager(bStore, user);
+
+        memberOption(memberMenu());
+
+    }
+
+    public int memberMenu(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nVälj ett alternativ:\n1.Låna bok\n2.Lämna tillbaka bok\n3.Säg upp medlemskap\n0.Logga ut");
+        System.out.print("Val: ");
+
+        return input.nextInt();
+    }
+
+    public void memberOption(int option) {
+        System.out.println("");
+        if (option == 1){
+            loanByMember();
+        }
+        else if (option == 2){
+            System.out.println("Lämna tillbaka bok metod");
+        }
+        else if (option == 3){
+            System.out.println("Säg upp medlemskap metod");
+        }
+        else if (option == 0){
+            Menu menu = new Menu();
+            menu.start();
+        }
+        else {
+            System.out.println("Inget gitligt val");
+            memberOption(memberMenu());
+        }
+    }
+
+    public void loanByMember(){
+        System.out.println("Ange ISBN för bok");
+        System.out.print("ISBN: ");
+        Scanner input = new Scanner(System.in);
+        int ISBN = input.nextInt();
+        bManager.loan(ISBN, user.getIDCode());
+        memberOption(memberMenu());
+    }
+
+}
