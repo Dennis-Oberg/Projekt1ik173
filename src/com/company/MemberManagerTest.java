9271ca68b71c4030e0e127164f25bf698eedf32c
Tobias Wendel <hansson101@gmail.com>
2021-05-26 09:48:44 +0200

    MemberManager test påbörjat

---
+++ src/com/company/MemberManagerTest.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemberManagerTest {
+
+    User testUser = new User(123,20211224, "Tobias", "Wendel",1);
+
+    @Test
+    public void test_checkActiveSuspension(){
+        MemberStoreStub memberStoreStub = new MemberStoreStub();
+        MemberManager cut = new MemberManager(testUser, memberStoreStub);
+
+        Boolean expected = false;
+
+
+        assertTrue(cut.checkActiveSuspension(testUser));
+
+    }
+}
