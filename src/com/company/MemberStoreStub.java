376944f15ae2cc1c5d1ab5b0ec74ddddb6c968c8
Tobias Wendel <hansson101@gmail.com>
2021-05-26 08:56:57 +0200

    MemberStoreStub gjord

--- src/com/company/MemberStoreStub.java
+++ src/com/company/MemberStoreStub.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class MemberStoreStub extends MemberStore{
 
     ArrayList<User> userList;
+    ArrayList<Integer> bannedMember;
 
     public MemberStoreStub(){
         userList = new ArrayList<>();
+        bannedMember = new ArrayList<Integer>();
 
 
         //ska bort senare
@@ -17,10 +19,16 @@ public class MemberStoreStub extends MemberStore{
         //userList.add(new User(4321,1,"Tobias", "Wendel", 5));
     }
 
-    public ArrayList<User> getMembers()
-    {
-        return userList; //databas kod
+    public Boolean login(int loginId) {
+
+        for (User user: userList){
+            if (user.getIDCode() == loginId){
+                return true;
+            }
+        }
+        return false;
     }
+
     public User getMemberById(int id){
 
         for (User m: userList){
@@ -30,6 +38,48 @@ public class MemberStoreStub extends MemberStore{
             }
         }
         return null;
+    }
+
+    public void creatNewMember(int ssn, String fName, String lName, int title) {
+
+        ArrayList<Integer> tempArr = new ArrayList<>();
+        for (User user: userList){
+            tempArr.add(user.getIDCode());
+        }
+
+        userList.add(new User(GenerateID.returnID(tempArr),ssn,fName,lName,title,
+                0,0,null,false));
+    }
+
+    public void removeMember(User user) {
+
+        for (User m: userList){
+            if (m.getIDCode() == user.getIDCode()){
+                userList.remove(m);
+            }
+        }
+    }
+
+    public void moveToBannedMember(User user) {
+        bannedMember.add(user.getSSN());
+    }
+
+    public void addSuspension(User user) {
+        user.suspended = true;
+    }
+
+    public void removeSuspension(User user) {
+        user.suspended = false;
+    }
+
+    public void addStrike(User user) {
+        user.strikes += 1;
+    }
+
+    public Integer[] checkSsn(int ssn) {
+
+        Integer[] lista = new Integer[bannedMember.size()];
+        return bannedMember.toArray(lista);
     }
 
 }
